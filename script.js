94dd81262f22ef001a7002dcbe3fe1eb3435d822
DzinToDev <maheshdgaikwad18@gmail.com>
2025-07-29 00:40:10 +0530

    page3 gooye effect done with shery js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function loadingAnimation() {
   },'h')
 
 }
-loadingAnimation()
+// loadingAnimation()
 
 function cursor() {
   document.addEventListener("mousemove", function (dets) {
@@ -121,4 +121,13 @@ function cursor() {
 
   Shery.makeMagnet(".nav-links-right h4, .left-logo-container #first-svg");
 }
-cursor();
+// cursor();
+
+function sheryAnimation(){
+  Shery.imageEffect(".img-div", {
+    style:5,
+    config:{"a":{"value":1.15,"range":[0,30]},"b":{"value":-0.94,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272667488697082},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.13,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.76,"range":[0,10]},"metaball":{"value":0.35,"range":[0,2],"_gsap":{"id":3}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+    gooey:true
+  })
+}
+sheryAnimation()
